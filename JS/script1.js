d166c17dc925f6cbae8ad698fe31dd4903ac956f
dheerud13 <rcharan10@gmail.com>
2017-09-02 22:42:26 +0530

    javascript

---
+++ JS/script1.js
@@ -0,0 +1,34 @@
+/*
+  This is Javascript code
+  */
+
+console.log('Javacript');
+
+console.log("Datatypes")      //Datatypes are Mutable.
+var name = "Dheeraj";  //datatype : String
+
+var age = 19;  //datatype : Number
+console.log(age); 
+
+var fullAge = true; //datatype : Boolean
+console.log(fullAge);
+
+//console.log(name + age); // adding string and number.This convert the number into string called type coercion.
+//console.log(age + age);  // adding numbers.
+
+var def; 
+console.log(def) // datatype : Undefined
+
+
+var pos = 'Student';
+var isMarried = false;
+console.log(name +" is a "+age+" years old and he is a "+pos+".Is he married? "+isMarried+".");
+
+var yourName = prompt("What is your name?");
+var age1 = prompt("your age:");   // prompt window
+age1 = age1 -1;
+console.log("Your age lastyear"+age1);
+
+console.log(yourName);
+alert(name +" is a "+age+" years old and he is a "+pos+".Is he married? "+isMarried+"."); //alert window
+
---
+++ JS/script2.js
@@ -0,0 +1,20 @@
+
+//operators
+
+var now =2017;
+var birthYear = now - 19;
+birthYear = now - 19 * 2;
+//2017 - 38
+
+console.log(birthYear);
+
+var math1 = math2 = (3+5) * 4 - 6;
+//math1 = math2 = 26
+math1++;
+math1 = math1 + 1;
+math2--;
+math2 *= 2;  
+console.log(math1);
+console.log(math2);
+
+
